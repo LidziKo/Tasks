0c124505b03c068a364a869b3b96e700a28b674a
LIDZIKO <lidia1473@gmail.com>
2022-02-04 14:29:11 +0300

    03022022 part 3

---
+++ First/src/Zero/Task04/Solution0410.java
@@ -0,0 +1,51 @@
+package Zero.Task04;
+import java.util.Scanner;
+
+/*
+В этой задаче нужно:
+
+        Считывать целые числа с клавиатуры до тех пор, пока не будет введено что-то другое: например, строка или символ.
+        Должно быть введено минимум два числа. Если введено меньше двух целых чисел, то такую ситуацию обрабатывать не нужно, а программа может быть завершена с ошибкой.
+        Вывести на экран второе по величине целое число после минимального из введенных с клавиатуры. Если таких чисел несколько, то необходимо вывести любое из них.
+        Минимальных чисел тоже может быть несколько.
+*/
+
+public class Solution0410 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstInt = scanner.nextInt();
+        int nextInt = scanner.nextInt();
+        if (firstInt > nextInt) {
+            int swap;
+            swap = firstInt;
+            firstInt = nextInt;
+            nextInt = swap;
+        }
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int x = scanner.nextInt();
+
+                if (firstInt == nextInt) {
+                    if (x > firstInt) {
+                        nextInt = x;
+                    }
+                    if (x < firstInt) {
+                        firstInt = x;
+                    }
+                }
+                if ((x > firstInt) && (x < nextInt)) {
+                    nextInt = x;
+                }
+                if (x < firstInt) {
+                    nextInt = firstInt;
+                    firstInt = x;
+                }
+            }
+            else {
+                System.out.println(nextInt);
+                break;
+            }
+        }
+    }
+}
