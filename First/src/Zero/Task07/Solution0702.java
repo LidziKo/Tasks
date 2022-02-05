5296653784278f5d6e68bb6633dd1d99e0744ccc
LIDZIKO <lidia1473@gmail.com>
2022-02-06 01:08:42 +0300

    06022022 try arrays

---
+++ First/src/Zero/Task07/Solution0702.java
@@ -0,0 +1,33 @@
+package Zero.Task07;
+
+/*В методе main(String[]) тебе нужно заполнить массив strings значениями.
+        Если индекс массива чётный — присвоить значение "Чётный" (ноль — цифра чётная), иначе присвоить "Нечётный".
+        Вывод текста в консоль в тестировании не участвует.*/
+
+public class Solution0702 {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < strings.length; i++) {
+            if (i % 2 == 0) {
+                strings[i] = EVEN;
+            }
+            else {
+                strings[i] = ODD;
+            }
+        }
+
+        System.out.print("index = 0");
+        System.out.println(" value = " + strings[0]);
+        System.out.print("index = 1");
+        System.out.println(" value = " + strings[1]);
+        System.out.print("index = 2");
+        System.out.println(" value = " + strings[2]);
+        System.out.print("index = 3");
+        System.out.println(" value = " + strings[3]);
+        System.out.print("index = 4");
+        System.out.println(" value = " + strings[4]);
+    }
+}
