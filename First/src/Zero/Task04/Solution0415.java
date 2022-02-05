2da4b9803fb006f4d94f70553a31fae6bf841885
LIDZIKO <lidia1473@gmail.com>
2022-02-05 23:03:05 +0300

    05022022 массивы

---
+++ First/src/Zero/Task04/Solution0415.java
@@ -0,0 +1,10 @@
+package Zero.Task04;
+
+/*В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов, размер которых равен 10.*/
+
+public class Solution0415 {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
