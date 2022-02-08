1a5a08c036ada56f80b40c0d27a5cffc856cc3aa
LIDZIKO <lidia1473@gmail.com>
2022-02-08 23:13:40 +0300

    09022022 try arrays

---
+++ First/src/Zero/Task05/Solution0506.java
@@ -0,0 +1,32 @@
+package Zero.Task05;
+import java.util.Scanner;
+
+/*Минимальное из N чисел
+        Чтобы выполнить эту задачу, тебе нужно:
+
+        Ввести с клавиатуры число N.
+        Считать N целых чисел и заполнить ими массив.
+        Найти минимальное число среди элементов массива и вывести в консоль.*/
+
+public class Solution0506 {
+    public static int[] array;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
