3cb281364a365000efebf45e4e8e52940c43b672
LIDZIKO <lidia1473@gmail.com>
2022-02-09 00:16:26 +0300

    09022022 try arrays

--- First/src/Zero/Task05/Solution0507.java
+++ First/src/Zero/Task05/Solution0507.java
@@ -1,7 +1,34 @@
 package Zero.Task05;
 
-public class Solution0507 {
-    public static void main(String[] args) {
+/*Максимальное из N чисел
+        В этой задаче тебе нужно:
 
+        Ввести с клавиатуры число N.
+        Считать N целых чисел и заполнить ими массив array.
+        Найти максимальное число среди элементов массива.*/
+
+import java.util.Scanner;
+
+public class Solution0507 {
+    public static int[] array;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
     }
 }
+
