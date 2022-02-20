b7d0f5df8ccb5a182e0232ebaf142de03b492364
LIDZIKO <lidia1473@gmail.com>
2022-02-20 14:31:29 +0300

    20022022 tasks

---
+++ First/src/Syntax/Task09/Solution0911.java
@@ -0,0 +1,27 @@
+package Syntax.Task09;
+
+/*Проинициализируй статические переменные следующими значениями:
+
+        partyFace — "\uD83E\uDD73"
+        balloon — "\uD83C\uDF88"
+        gift — "\uD83C\uDF81"
+        partyPopper — "\uD83C\uDF89"
+        cake — "\uD83C\uDF82"*/
+
+public class Solution0911 {
+    public static String partyFace = "\uD83E\uDD73";
+    public static String balloon = "\uD83C\uDF88";
+    public static String gift = "\uD83C\uDF81";
+    public static String partyPopper = "\uD83C\uDF89";
+    public static String cake = "\uD83C\uDF82";
+
+    public static void main(String[] args) {
+        printCongratulation();
+    }
+
+    public static void printCongratulation() {
+        String happyBirthday = "С днем рождения!";
+        String congratulation = partyFace + balloon + partyPopper + cake + gift;
+        System.out.println(happyBirthday + congratulation);
+    }
+}
