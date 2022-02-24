ed7cd0b2a0516b0faa75284f11d32510ca784f94
LIDZIKO <lidia1473@gmail.com>
2022-02-25 02:42:52 +0300

    25022022

---
+++ First/src/Syntax/Task05/Solution0518.java
@@ -0,0 +1,16 @@
+package Syntax.Task05;
+
+/*Реализуй метод main(String[]), который сортирует массив array по возрастанию.
+        Для сортировки массива используй метод Arrays.sort(int[]).*/
+
+import java.util.Arrays;
+
+public class Solution0518 {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
