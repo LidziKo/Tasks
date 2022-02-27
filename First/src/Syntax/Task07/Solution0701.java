34cb68241d724bbd8cd52a07f97e45619ebc40b5
LIDZIKO <lidia1473@gmail.com>
2022-02-27 23:40:43 +0300

    27022022

---
+++ First/src/Syntax/Task07/Solution0701.java
@@ -0,0 +1,16 @@
+package Syntax.Task07;
+
+/*Как тебе уже известно, всего в языке Java 8 примитивных типов.
+        Создай по одной переменной каждого примитивного типа в классе Solution.
+        Переменные не должны находиться в каком-либо методе.*/
+
+public class Solution0701 {
+    int i = 1;
+    byte by = 4;
+    char c = 'a';
+    long l = 1234567890;
+    boolean b = true;
+    short s = 2;
+    float f = 1.0F;
+    double d = 1.0;
+}
