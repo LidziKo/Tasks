4a70e615a714ca3107ba5b72e860113ad1f332de
LidziKo <lidia1473@gmail.com>
2022-04-11 23:24:06 +0300

    11042022 exeptions

---
+++ First/src/syntax/task14/task1416/Solution.java
@@ -0,0 +1,20 @@
+package syntax.task14.task1416;
+
+/* 
+Логирование стектрейса
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            dangerousMethod();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void dangerousMethod() throws Exception {
+        throw new Exception("Mu-ha-ha!");
+    }
+}
