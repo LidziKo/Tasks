1ab253cce454a73da2a7a09f42104d88f0b34776
LidziKo <lidia1473@gmail.com>
2022-04-14 22:28:04 +0300

    14042022 IO

---
+++ First/src/task15/task1524/Solution.java
@@ -0,0 +1,39 @@
+package task15.task1524;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+/* 
+Перепутанные байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            FileInputStream fileInputStream = new FileInputStream(scanner.nextLine());
+            FileOutputStream fileOutputStream = new FileOutputStream(scanner.nextLine());
+
+            byte[] arrayIn = fileInputStream.readAllBytes();
+            byte[] arrayOff = new byte[arrayIn.length];
+
+            for (int i = 0; i < arrayIn.length; i += 2) {
+                if (i < arrayIn.length - 1) {
+                    arrayOff[i] = arrayIn[i + 1];
+                    arrayOff[i + 1] = arrayIn[i];
+                } else {
+                    arrayOff[i] = arrayIn[i];
+                }
+            }
+            fileOutputStream.write(arrayOff);
+
+            fileInputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
+    }
+}
---
+++ First/src/task15/task1525/Solution.java
@@ -0,0 +1,26 @@
+package task15.task1525;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+/* 
+Что-то не копируется...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in);
+             FileInputStream fis = new FileInputStream(scanner.nextLine());
+             FileOutputStream fos = new FileOutputStream(scanner.nextLine())) {
+            int size = 1024;
+            byte[] buffer = new byte[size];
+            while (fis.available() > 0) {
+                int read = fis.read(buffer);
+                fos.write(buffer, 0, read);
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
